0bf9522ba7e6ccef309396f9bb8af160c9b80f7c
Meryem236 <143514086+Meryem236@users.noreply.github.com>
2024-07-21 23:49:08 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const envelope = document.querySelector('.envelope-wrapper');
+
+    envelope.addEventListener('click', () => {
+        envelope.classList.toggle('flap');
+    });
+
+   
+});
